ab4b2804d995be25cb5a26d9eaeac17c1e7d4967
TudorParker <101466387+TudorParker@users.noreply.github.com>
2022-06-28 05:14:30 +0100

    Create 7-islower.py

---
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# Author - Charles Ekpunobi
+
+def islower(c):
+    """Function checks for lowercase characters."""
+    if ord(c) >= 97 and ord(c) <= 122:
+        return True
+    else:
+        return False
